0cd6b5d90aaca9018460983c5a85edad74d64ebe
Ayush <29029116+aydwi@users.noreply.github.com>
2018-03-10 22:34:50 +0530

    Restructure long lines

--- extractdata.py
+++ extractdata.py
@@ -1,14 +1,17 @@
-# An independent program to fetch and display explanation of an xkcd comic by providing its serial number
+# An independent program to fetch and display explanation of
+# an xkcd comic by providing its serial number
 # The explanation is extracted from http://explainxkcd.com
 # License: MIT License
 
 
 import requests
 import bs4
+
 from bs4 import BeautifulSoup
 
 xkcd_id = 1024
-# Replace xkcd_id with the serial number of xkcd you want the explanation for
+# Replace xkcd_id with the serial number of xkcd you want the
+# explanation for
 
 url = 'http://www.explainxkcd.com/wiki/index.php/' + str(xkcd_id)
 
@@ -25,7 +28,8 @@ def main():
                 break
             if (tag.name == 'h3'):
                 tag = tag.nextSibling
-                # This skips the contents of the 'h3' tag, which are irrelevant to the explanation
+                # This skips the contents of the 'h3' tag, which
+                # are irrelevant to the explanation
             else:
                 data = data + '\n' + tag.text
                 tag = tag.nextSibling
